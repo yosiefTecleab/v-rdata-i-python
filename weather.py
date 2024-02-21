7c71984987dd5a8e9dbd7b0bcb70eabeea7453c6
yosiefTecleab <yosieftecleab@gmail.com>
2024-02-21 17:41:34 +0100

    use source id at index 1, i.e blindern

--- weather.py
+++ weather.py
@@ -26,7 +26,7 @@ def get_source_id(place):
       if place.upper() in key:
         stations.append(source_id_dict[key])
 
-    return stations[0]
+    return stations[1]
 
 
 
@@ -43,28 +43,33 @@ day, month, year = date_string.split('.')
 DATE = f"{year}-{month}-{day}"
 
 #SOURCE_ID = SOURCE_IDS_DICT["Oslo"]
+#source='sn18700'
 SOURCE_ID = get_source_id(place_string)
 
 print(f'Temperatur for Oslo {DATE} ')
 
 for HOUR in range(24):
-  HOUR_STR = str(HOUR).zfill(2)  # Pad single digits with leading zero
-  REFERENCE_TIME = f"{DATE}T{HOUR_STR}:00:00Z"
-  URL = f"https://frost.met.no/observations/v0.jsonld?sources={SOURCE_ID}&referencetime={REFERENCE_TIME}&elements=air_temperature"
-  response = requests.get(URL, auth=(CLIENT_ID, ''))
-  data = response.json()
-
-  hour = data['data'][0]['referenceTime']
-  hourly_temperature = data['data'][0]['observations'][0]['value']
-
-  #extract hour in 4 digit format
-  hour_4digits = hour[hour.index('T') + 1:hour.index('T') + 6]
-
-  #e.g  Kl 00:00 10 grader
-  output = f'Kl {hour_4digits} {hourly_temperature} grader'
-
-  print(output)
+    HOUR_STR = str(HOUR).zfill(2)  # Pad single digits with leading zero
+    REFERENCE_TIME = f"{DATE}T{HOUR_STR}:00:00Z"
 
 
+    URL = f"https://frost.met.no/observations/v0.jsonld?sources={SOURCE_ID}&referencetime={REFERENCE_TIME}&elements=air_temperature"
+
+
+    response = requests.get(URL, auth=(CLIENT_ID, ''))
+    data = response.json()
+
+
+    hour = data['data'][0]['referenceTime']
+    hourly_temperature = data['data'][0]['observations'][0]['value']
+
+    #extract hour in 4 digit format
+    hour_4digits = hour[hour.index('T') + 1:hour.index('T') + 6]
+
+    #e.g  Kl 00:00 10 grader
+    output = f'Kl {hour_4digits} {hourly_temperature} grader'
+
+    print(output)
+    print(SOURCE_ID)
 
 
